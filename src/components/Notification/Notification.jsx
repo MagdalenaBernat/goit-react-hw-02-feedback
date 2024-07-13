967c9bedbd0e0608d6b9998d957cfcfa13503330
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-07-13 17:54:00 +0200

    added components

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,14 @@
+
+
+export const NotificationMsg = ({ message = "There is no feedback" }) => {
+    return (
+        <>
+            if () {
+                <p>{message}</p>
+            } else {
+                //cały komponent ze statystykami 
+            }
+
+        </>
+    )
+}
