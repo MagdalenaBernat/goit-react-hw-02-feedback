b5dc3926548bd46f510598fff35dba95cbb62831
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-07-14 21:14:30 +0200

    correct import

--- src/components/Contacts/Phonebook/Phonebook.jsx
+++ src/components/Contacts/Phonebook/Phonebook.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import FindContact from "./FindContact/FndContact"
+import { FindContact } from "../FindContact/FindContact"
 
 export const Phonebook = () => {
 
