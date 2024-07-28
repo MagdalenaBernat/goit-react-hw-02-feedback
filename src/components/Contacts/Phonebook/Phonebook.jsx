c497b1dd818523f8a14aecfb543fea7ce86b8d87
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-07-28 17:23:17 +0200

    update

--- src/components/Contacts/Phonebook/Phonebook.jsx
+++ src/components/Contacts/Phonebook/Phonebook.jsx
@@ -18,7 +18,7 @@ export const Phonebook = () => {
                 <input
                     type="text"
                     name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    pattern="^[a-zA-Zа-яА-Я]+((['\s\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
                 />
